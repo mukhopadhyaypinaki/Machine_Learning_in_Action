13a849560be5cf19c81d9d54e267af23d80aca09
Pinaki Mukhopadhyay <mukhopadhyaypinaki@gmail.com>
2020-08-23 19:31:14 +0200

    completed learning

--- movieClassifier.py
+++ movieClassifier.py
@@ -9,7 +9,7 @@ def createDataSet():
 
 
 group, labels = createDataSet()
-print(group, labels)
+#print(group, labels)
 
 
 def classify0(inX, dataSet, labels, k):
@@ -23,9 +23,12 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-        sortedClassCount = sorted(classCount.items(),
+
+    sortedClassCount = sorted(classCount.items(),
                                   key=operator.itemgetter(1), reverse=True)
-        return sortedClassCount[0][0]
+
+    return sortedClassCount[0][0]
 
 
-print(classify0([0, 0], group, labels, 3))
+
+print(classify0([0.544, 0.52], group, labels, 3))
